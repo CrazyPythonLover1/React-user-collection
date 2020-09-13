ef765f144538518edccd9ee37446e362a273798b
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-14 01:27:27 +0600

    increase and decrease with reducer

--- src/components/ReducerCount/ReducerCount.js
+++ src/components/ReducerCount/ReducerCount.js
@@ -3,7 +3,14 @@ import React, { useReducer } from 'react';
 const initailState = {count: 0}
 
 const reducer = (state,action) =>{
-    
+    switch(action.type){
+        case 'INCREMENT':
+            return {count: state.count+1}
+        case 'DECREMENT':
+            return {count: state.count-1}
+        default:
+            return state;
+    }
 }
 
 const ReducerCount = () => {
@@ -12,7 +19,9 @@ const ReducerCount = () => {
 
     return (
         <div>
-            <h1> This is reducer </h1>
+            <h1> This is reducer {state.count} </h1>
+            <button onClick={()=>dispatch({type: 'INCREMENT'})} >Increment</button>
+            <button onClick={()=>dispatch({type: 'DECREMENT'})} >Decrement</button>
         </div>
     );
 };
